4bc6eee303182ce8f40fb001283c7d6f0c6d1565
theinpu <theinpu@gmail.com>
2013-12-28 07:44:04 +0400

    начинаем

---
+++ aascms/Mongo/Mongo.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * User: inpu
+ * Date: 28.12.13
+ * Time: 2:26
+ */
+
+namespace aascms\Mongo;
+
+class Mongo {
+
+} 
---
+++ aascms/tests/MongoTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: inpu
+ * Date: 28.12.13
+ * Time: 7:40
+ */
+
+namespace aascms\tests;
+
+class MongoTest extends \PHPUnit_Framework_TestCase {
+
+    public function testCreate()
+    {
+
+    }
+}
+ 
