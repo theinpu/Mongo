9c64d7c1c892062005bfe90777c53052664ee14c
theinpu <theinpu@gmail.com>
2013-12-28 08:12:27 +0400

    коннект работает, коллекция получается, инфа пишется

--- aascms/Mongo/Mongo.php
+++ aascms/Mongo/Mongo.php
@@ -7,6 +7,61 @@
 
 namespace aascms\Mongo;
 
+use aascms\Config\ConfigManager;
+
 class Mongo {
+
+    /**
+     * @var \MongoClient
+     */
+    private static $mongo;
+    /**
+     * @var \MongoDB
+     */
+    private static $mongoDB;
+    /**
+     * @var \MongoCollection[]
+     */
+    private static $storedCollections = array();
+
+    /**
+     * @param $collection
+     * @throws \Exception
+     * @return \MongoCollection
+     */
+    public static function getCollection($collection) {
+        self::checkMongo();
+        if(isset(self::$storedCollections[$collection])) {
+            return self::$storedCollections[$collection];
+        }
+        self::$storedCollections[$collection] = self::$mongoDB->selectCollection($collection);
+        return self::$storedCollections[$collection];
+    }
+
+    private static function checkMongo()
+    {
+        $cfg = ConfigManager::get('cfg/mongo.json');
+        if (is_null(self::$mongo)) {
+            $connectString = 'mongodb://';
+            $options = array('connect' => true);
+            $connectString .= $cfg->get('host');
+            try {
+                $connectString .= ':'.$cfg->get('port');
+            }
+            catch(\InvalidArgumentException $e) {
+                //ничего не делаем. например, в хосте указан сокет
+            }
+            $options = array_merge($options, $cfg->get('options'));
+
+            self::$mongo = new \MongoClient($connectString, $options);
+        } elseif(!self::$mongo->connected) {
+            if(!self::$mongo->connect()) {
+                throw new \RuntimeException();
+            }
+        }
+        if(is_null(self::$mongoDB)) {
+            self::$mongoDB = self::$mongo->selectDB($cfg->get('database'));
+        }
+    }
 
 } 
--- aascms/tests/MongoTest.php
+++ aascms/tests/MongoTest.php
@@ -7,11 +7,19 @@
 
 namespace aascms\tests;
 
+use aascms\Mongo\Mongo;
+
 class MongoTest extends \PHPUnit_Framework_TestCase {
 
-    public function testCreate()
+    public function testMongo()
     {
-
+        $collection = Mongo::getCollection("test");
+        $this->assertInstanceOf('\MongoCollection', $collection);
+        $testItem = array('test' => 'test');
+        $this->assertFalse(!$collection->save($testItem));
+        $savedItem = $collection->findOne();
+        $this->assertEquals($testItem, $savedItem);
+        $this->assertFalse(!$collection->drop());
     }
 }
  
